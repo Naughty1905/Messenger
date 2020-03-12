aa549b5abded52d7ee10bb6eca4a4e9f0f1cf5de
Airat <parkourtes@gmail.com>
2020-03-12 15:48:21 +0300

    fix unread messages

--- front/src/components/MessageList/index.js
+++ front/src/components/MessageList/index.js
@@ -1,6 +1,7 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useMemo, useState, useCallback } from 'react';
 import Compose from '../Compose';
 import Toolbar from '../Toolbar';
+import { keys } from 'lodash'
 import { database } from '../../Firebase';
 import ToolbarButton from '../ToolbarButton';
 
@@ -18,6 +19,11 @@ import renderMessages from './renderMessage';
 
 const MessageList = props => {
   const { message, messages, user, chat, chats } = props;
+  // if (chat) {
+  //   const currentMes = chats[chat]['messages'];
+  //   console.log(currentMes)
+  // }
+  // const [currentMessages, setCurrentMessages] = useState(null);
   const messagesEndRef = useRef(null);
   const scrollToBottom = () => {
     messagesEndRef.current.scrollIntoView({ behavior: "auto" })
@@ -30,23 +36,48 @@ const MessageList = props => {
     }
   }, [message])
 
+  // useEffect(() => {
+  //   if (chat) {
+  //     setCurrentMessages(Object.assign({}, currentMessages, {
+  //       ...chats[chat]['messages']
+  //     }))
+  //   }
+  // }, [chat])
+
+  // if (chat) {
+  //   debugger
+  //   if (chat) {
+  //     if (currentMessages) {
+  //       useMemo(() => {
+  //         setCurrentMessages(Object.assign({}, currentMessages, {
+  //           ...chats[chat]['messages']
+  //         }))
+  //       }, [chats[chat]['messages']])
+  //     }
+  //   }
+
+  // }
+
+
   useEffect(() => {
     if (chat) {
-      props.setMessages(chats[chat]['messages'])
+      const chatRef = database.ref(`chats/${chat}`).limitToLast(100);
+      chatRef.on('value', snapshot => {
+        if (snapshot.val()) {
+          const messages = { ...snapshot.val() };
+          const keysOfMessages = keys(messages);
+          const unreadMessages = keysOfMessages.reverse().filter(key => !messages[key].isSeen && messages[key].user !== user);
+          unreadMessages.map(unreadMessage => {
+            messages[unreadMessage].isSeen = true;
+            database.ref(`chats/${chat}/${unreadMessage}`).update(messages[unreadMessage]);
+          })
+          props.setMessages(messages)
+        }
+      })
     }
-    // if (chat) {
-    //   const chatRef = database.ref(`chats/${chat}`).limitToLast(100);
-    //   chatRef.on('value', snapshot => {
-    //     const getChats = snapshot.val();
-
-    //     if (getChats) {
-    //       const messages = Object.values(getChats);
-    //       props.setMessages(messages)
-    //     }
-    //   })
-
-    // }
   }, [chat])
+
+
 
   useEffect(scrollToBottom, [messages]);
 
