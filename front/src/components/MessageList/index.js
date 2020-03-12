57053c45dfaf0d633fae8fff28f5e8ab2e717e36
Artem <artembeluaev98@mail.ru>
2020-03-12 16:13:21 +0300

    Refactoring

--- front/src/components/MessageList/index.js
+++ front/src/components/MessageList/index.js
@@ -46,7 +46,6 @@ const MessageList = props => {
   // }, [chat])
 
   // if (chat) {
-  //   debugger
   //   if (chat) {
   //     if (currentMessages) {
   //       useMemo(() => {
@@ -101,18 +100,16 @@ const MessageList = props => {
       } */}
       <div className="messages-bottom" ref={messagesEndRef} style={{ marginBottom: '40px' }} />
 
-      <>
-        {
-          isAvailableToWrite && <Compose rightItems={[
-            <ToolbarButton key="photo" icon="ion-ios-camera" />,
-            <ToolbarButton key="image" icon="ion-ios-image" />,
-            <ToolbarButton key="audio" icon="ion-ios-mic" />,
-            <ToolbarButton key="money" icon="ion-ios-card" />,
-            <ToolbarButton key="games" icon="ion-logo-game-controller-b" />,
-            <ToolbarButton key="emoji" icon="ion-ios-happy" />
-          ]} />
-        }
-      </>
+
+      <Compose rightItems={[
+        <ToolbarButton key="photo" icon="ion-ios-camera" />,
+        <ToolbarButton key="image" icon="ion-ios-image" />,
+        <ToolbarButton key="audio" icon="ion-ios-mic" />,
+        <ToolbarButton key="money" icon="ion-ios-card" />,
+        <ToolbarButton key="games" icon="ion-logo-game-controller-b" />,
+        <ToolbarButton key="emoji" icon="ion-ios-happy" />
+      ]} />
+
     </div >
   );
 }
