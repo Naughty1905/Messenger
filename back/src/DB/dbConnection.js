ebc567df1a368d84de0a731fb0fd83e389d40762
Artem <artembeluaev98@mail.ru>
2020-03-13 15:28:44 +0300

    Prepare for deploy

--- back/src/DB/dbConnection.js
+++ back/src/DB/dbConnection.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
 
 const DBconnection = () => {
-  const connectionUrl = `mongodb+srv://${process.env.DB_LOGIN}:${process.env.DB_PASSWORD}@cluster0-rskpa.mongodb.net/test?retryWrites=true&w=majority`;
-  return mongoose.connect(connectionUrl, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
-
+  return mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 }
 
 module.exports = DBconnection;
